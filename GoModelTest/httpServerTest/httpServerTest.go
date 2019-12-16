6c00883260759dbb0c15291c1c337016c87291e3
chuangangshen <chuangangshen@deepglint.com>
2019-12-16 16:51:32 +0800

    http post file and md5 sum

--- GoModelTest/httpServerTest/httpServerTest.go
+++ GoModelTest/httpServerTest/httpServerTest.go
@@ -5,6 +5,12 @@ import (
 	"io/ioutil"
 	"fmt"
 	"io"
+	"github.com/julienschmidt/httprouter"
+	"os"
+	"strings"
+	"path/filepath"
+	"crypto/md5"
+	"time"
 )
 
 func main() {
@@ -12,6 +18,77 @@ func main() {
 	// 方法二
 	// router.StartTest(":5678")
 	*/
+	// run()
+	runApp()
+}
+
+func runApp() {
+	router := httprouter.New()
+	router.PUT("/upload_file", handleUploadFile)
+	http.ListenAndServe("127.0.0.1:19999", router)
+}
+
+func handleUploadFile(rw http.ResponseWriter, req *http.Request, p httprouter.Params) {
+	var err error
+	const _24K = (1 << 20) * 24
+	if err = req.ParseMultipartForm(_24K); err != nil {
+		http.Error(rw, err.Error(), http.StatusForbidden)
+		return
+	}
+
+	// get md5 value passed with request
+	values, ok := req.MultipartForm.Value["md5"]
+	if !ok || len(values) < 1 {
+		http.Error(rw, "no md5 value passed", http.StatusBadRequest)
+		return
+	}
+	md5Value := values[0]
+
+	// get tar file passed with PostForm request
+	var fileAbsPath, md5Hash string
+	for _, fileHeaders := range req.MultipartForm.File {
+		if len(fileHeaders) != 1 {
+			http.Error(rw, "file error", http.StatusBadRequest)
+			return
+		}
+		for _, fileHeader := range fileHeaders {
+			file, _ := fileHeader.Open()
+			defer file.Close()
+			filename := fmt.Sprintf("%s-%d.tar.gz", strings.TrimSuffix(fileHeader.Filename, ".tar.gz"), time.Now().Unix())
+			fileAbsPath = filepath.Join("/Users/hpu_scg/gocode/src/temp/GoModelTest/httpServerTest", filename)
+			f, err := os.Create(fileAbsPath)
+			defer f.Close()
+			if err != nil {
+				fmt.Println(err)
+				http.Error(rw, "file error:" + err.Error(), 501)
+				return
+			}
+			s := make([]byte, 4096)
+			for {
+				switch nr, err := file.Read(s[:]); true {
+				case nr < 0:
+					fmt.Fprintf(os.Stderr, "cat: error reading: %s\n", err.Error())
+					os.Exit(1)
+				case nr == 0: // EOF
+					goto L
+				case nr > 0:
+					fmt.Println(nr)
+					f.Write(s[0:nr])
+				}
+			}
+			L:
+			data, err := ioutil.ReadFile(fileAbsPath)
+			md5Hash = fmt.Sprintf("%x", md5.Sum(data))
+			fmt.Println(md5Hash)
+			if md5Hash != md5Value {
+				http.Error(rw, "bad md5 value caculated", http.StatusBadRequest)
+				return
+			}
+		}
+	}
+}
+
+func run() {
 	http.HandleFunc("/event", HandedRequest)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
