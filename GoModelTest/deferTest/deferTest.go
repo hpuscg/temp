0703508e3113d2afaf1e47b5fdd96dab02f9366f
chuangangshen <chuangangshen@deepglint.com>
2020-02-28 17:19:29 +0800

    add defer test

--- GoModelTest/deferTest/deferTest.go
+++ GoModelTest/deferTest/deferTest.go
@@ -12,8 +12,33 @@ import (
 )
 
 func main() {
-	testDefer()
+	// testDefer()
+	defer_call()
 }
+
+func defer_call() {
+
+	defer func() {
+		if err := recover();err != nil {
+			fmt.Println(err) //err 就是panic传入的参数
+		}
+		fmt.Println("打印前")
+	}()
+
+	defer func() { // 必须要先声明defer，否则recover()不能捕获到panic异常
+
+		fmt.Println("打印中")
+	}()
+
+
+	defer func() {
+
+		fmt.Println("打印后")
+	}()
+	panic("触发异常")
+}
+
+
 
 func testDefer()  {
 	defer func() {
